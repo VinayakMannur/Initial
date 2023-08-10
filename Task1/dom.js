cbf284d7caef598c82e08631f8bd541b1553cd31
VinayakMannur <Vinayakmannur20@gmail.com>
2023-08-10 21:26:13 +0530

    get element by class

--- Task1/dom.js
+++ Task1/dom.js
@@ -30,14 +30,26 @@ addItem.innerHTML = '<strong>Add Item</strong>';
 addItem.style.color = 'green';
 
 
-const items = document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent = 'Hello 2';
-// items[2].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green';
+// const items = document.getElementsByClassName('list-group-item');
+// // console.log(items);
+// // console.log(items[1]);
+// // items[1].textContent = 'Hello 2';
+// // items[2].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green';
 
 
-for(var i=0; i<items.length ;i++){
-    items[i].style.fontWeight = 'bold';
+// for(var i=0; i<items.length ;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+
+const li = document.getElementsByTagName('li');
+// console.log(li)
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[2].style.fontWeight = 'bold';
+li[2].style.backgroundColor = 'green';
+for(var i=0; i<li.length ;i++){
+    li[i].style.fontWeight = 'bold';
 }
+
